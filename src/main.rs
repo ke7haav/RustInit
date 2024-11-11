de9af76de083f227c30966e933d2cf3230c0c108
ke7haav <ke5havmg007@gmail.com>
2024-11-11 18:11:04 +0530

    beg

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,256 @@
+// use std::string;
 
+// use std::clone;
+
+trait Pintable{
+    fn print(&self){
+
+    }
+}
 struct Rectangle{
     width:u32,
     height:u32
 }
 
-// We can use multiple implementation 
-impl Rectangle {
-    //function inside implementation is an internal function 
-    fn print_area(&self) {
-        println!("Area of Rectangle is {}",self.width*self.height);
-    }
-
-    fn is_squre(&self) ->bool{
-         self.width==self.height
+impl Pintable for Rectangle{
+    fn print(&self){
+      println!("Rectangle: {} x {}",self.width ,self.height)
     }
 }
 
+trait HasArea {
+    fn area(&self) ->f64;
+}
 
+struct Square {
+    side:f64
+}
+
+impl HasArea for Square {
+    fn area(&self) ->f64 {
+        self.side *self.side
+    }
+}
+
+fn print_area <T:HasArea>(shape:T){
+    println!("Area of Shape is {}", shape.area());
+}
 fn main() {
 
-    let myrect = Rectangle{width:20,height:40};
+    let rect1 = Rectangle{width:43,height:7};
+    rect1.print();
 
-    myrect.print_area();
-    println!("{}",myrect.is_squre());
-
-    let mut _person = Person{
-        name:String::from("Ravi"),
-        email:String::from("ravi@gmail.com"),
-        age:43,
-        gender:Gender::NonBinary
+    let squre1 = Square{
+        side:9.30
     };
 
-    println!("Struct {:?}",_person);
-    let result =call(81);
-    println!("{:?}",result);
-    touplestruct();
-    stackmemory();
-    heapmemory();
-    vestors();
+    print_area(squre1);
+
+    const USERS:u32=100;
+    const USER_AVG:f64=42.4;
+
+    println!("Number Of Users are {}",USERS);
+    println!("Average of Users are {}",USER_AVG);
+    let a= 45; // Variables are by-default immutable 
+    let mut _b=66;
+    println!("{a}");
+    println!("{}",a);
+    println!("{}",_b);
+
+    _b= 90;
+    println!("{}",_b);
+
+    let c ="String";
+    println!("{}",c);
+
+    first_fn();
+    function_with_params(8);
+    multiple_params(99,"hochimin");
+    ex();
+    let mut _return_val = return_value();
+    println!("returned Value is -{}",_return_val);
+
+    data_type();
+    conditions();
+    loops();
+    ownership();
+    reference();
+    structs();
+
+    printcars(CarTypes::SUV);
+    // println!("{:?}",_x);
 }
 
-
-#[derive(Debug)]
-enum Gender{
-    Binary,
-    NonBinary,
-    Asexual
+fn first_fn(){
+    println!("new Function")
 }
 
-#[derive(Debug)]
-struct Person{
-    name:String,
-    email: String,
-    age:u32,
-    gender: Gender
+fn function_with_params(x:i32){
+  println!("The Value in the param {}",x);
 }
 
-//Option Enums - Default Enums provided by rust 
+fn multiple_params(x: i32, y: &str){
 
-/*
-enum Option<T>{
-Some(T)
+    println!("first Param {}",x);
+    println!("Second Param {}",y);
 }
-*/
 
-fn call(no:i32)->Option<bool>{
-    if no%2 ==0 {
-        Some(true)
-    }else{
-        None
+//expression
+fn ex(){
+    let y ={
+        let x= 10;
+        x+6
+    };
+  println!("value inside expression- {}",y)
+}
+
+fn return_value() -> i32{
+    98+57
+}
+
+fn data_type() {
+    //scalar Types - types where we can store single value
+    //Integer, boolean ,Floating,Char, Strings
+
+    //Compound Types - types where we store multiple data at a time 
+    // Array,tuples, dictionary
+
+
+    //tuple
+    let tup =(8,5,6);
+    println!("{:?}",tup);  // :? is default formatter 
+    println!("{}",tup.2);
+
+    let mut _tup2:(i32,u8,f64) = (4,6,7.3);
+    // Here we can change the value in tuple as it is mutable but we can't add the value in the tuple
+
+    println!("{:?}",_tup2);
+    _tup2.2 = 8.9;
+    println!("{:?}",_tup2);
+
+    // Array - is a collection of multiple values
+    let mut _arr =[7,3,40,0,1];
+    println!("This is the whole Array - {:?}",_arr);
+    println!("Fourth Element of Array {}",_arr[3]);
+
+}
+
+fn conditions(){
+    let no = 7;
+    if no<7{
+        println!("Less Than 7")
+    }else if no==7{
+        println!("Equal to 7")
+    }else {
+        println!("Greater Than 7")
+    }
+
+    // ShortEnd If Condition
+    let _condition = false;
+    let number = if _condition {5} else{0};
+    println!("Let's see the number - {}", number)
+}
+
+fn loops(){
+//Types of Loops 
+//loops , for, while
+let mut _x =10;
+loop {
+    println!("Print Till 10- {}",_x);
+
+    if _x == 11 || _x==10{
+      break;
     }
 }
 
-
-fn touplestruct(){
-    struct User(u8,f64,String);
-
-    // let mut _user1 = User(60,80,"Hisgenberg".to_string());
-    let mut _user1 = User(60,80.55,String::from("Hisgenberg"));
-
-    println!("{} -- {} -- {}",_user1.0,_user1.1,_user1.2);
+while _x!=0{
+    println!(" Number-{}",_x);
+    _x-=1;
 }
 
-//Stack Memory 
+let mut _arr=[2,3,5,7,1];
+let mut _index = 0;
 
-fn stackmemory(){
-    // Stack Memory are of fixed size 
-    let x=6; 
-    let y=8;
+while _index<3 {
+    println!("Value of Index is {}",_arr[_index]);
+    _index+=1;
+}
 
-    let sum = add(x,y);
-    println!("Sum of {} and {} is {}",x,y,sum);
 
-    fn add(a:i32,b:i32) ->i32{
-      a+b
+for _y in 1..11{  // y=0;y<11;y++ print y expect 5 
+    if _y==5{
+        continue;
     }
-
-    // x,y, sum Uses Stack Memory 
+    println!("y is ={}",_y);
+}
+    
 }
 
-fn heapmemory(){
-    //Heap memory is a dynamically allocated memory 
-    let mut v:Vec<i32> =Vec::new();
+fn ownership(){
+    let x=90;
+    println!("value of x - {}",x);
+    let y=x;
+    println!("value of Y - {}",y);
+  
+    //------------------------------
 
-    v.push(1);
-    v.push(7);
-    v.push(-4);
-    v.push(0);
-    println!("{:?}",v);
+    let a =String::from("Koder");
+    // let b:&str="KODER";
+    let b= a.clone();// clone funciton only works with string and Arrays
+
+    println!("{}-----{}",a,b);
 }
 
- //Vestors 
+fn reference(){
+    let mut _x=10;
+    let z = &mut _x;
+    *z +=1;
+    println!("value of z is - {}",z);
+    println!("value of x is - {}",_x);
 
- fn vestors(){
-    let mut _a = vec![1,2,3,4,5,6];
+}
 
-    _a.push(74);
-    println!("{:?}",_a);
-    let mut _b = Vec::<u32>::new();// Declearing an empty Vector 
- }
+struct User{
+    name :String,
+    company: String,
+    age:u8
+}
+
+fn structs(){
+
+    let mut _u1 = User{
+        name:String::from("Asish"),
+        company:String::from("WazirX"),
+        age:26
+    };
+
+    println!("Name {} Company {} Age {}", _u1.name,_u1.company, _u1.age);
+    _u1.age=27;
+    println!("Age Updated {}", _u1.age);
+
+}
+
+enum CarTypes {
+    Hatchback,
+    Sedan,
+    SUV,
+    MUV
+}
+
+fn printcars(car:CarTypes){
+match car {
+    CarTypes::Hatchback=>{
+        println!("Small car Segment")
+    }
+    CarTypes::MUV=>{
+        println!("MultiPurpose Car")
+    }
+    CarTypes::SUV=>{
+        println!("Sports Utility ")
+    }
+    CarTypes::Sedan=>{
+        println!("Luxury Cars")
+    }
+    
+}
+}
